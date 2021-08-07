8c2447b8045266551164cef450badf746deffbc3
papy439 <82462178+papy439@users.noreply.github.com>
2021-08-07 19:00:54 +0700

    Add files via upload

---
+++ admin/html/ngaydkonl.php
@@ -0,0 +1,281 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords"
+        content="wrappixel, admin dashboard, html css dashboard, web dashboard, bootstrap 5 admin, bootstrap 5, css3 dashboard, bootstrap 5 dashboard, materialpro admin bootstrap 5 dashboard, frontend, responsive bootstrap 5 admin template, materialpro admin lite design, materialpro admin lite dashboard bootstrap 5 dashboard template">
+    <meta name="description"
+        content="Material Pro Lite is powerful and clean admin dashboard template, inpired from Bootstrap Framework">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Ung Dung Ho Tro Quan Ly Hien Mau</title>
+    <link rel="canonical" href="https://www.wrappixel.com/templates/materialpro-lite/" />
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/logo.png">
+    <!-- chartist CSS -->
+    <link href="../assets/plugins/chartist-js/dist/chartist.min.css" rel="stylesheet">
+    <link href="../assets/plugins/chartist-js/dist/chartist-init.css" rel="stylesheet">
+    <link href="../assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css" rel="stylesheet">
+    <!--This page css - Morris CSS -->
+    <link href="../assets/plugins/c3-master/c3.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.min.css" rel="stylesheet">
+    
+</head>
+
+<body>
+    <?php
+        include "../config.php";
+    ?>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <header class="topbar" data-navbarbg="skin6" style="background-color: #6CA6CD;">
+            <?php include "../include/header.php"; ?>
+        </header>
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <aside class="left-sidebar" data-sidebarbg="skin6">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <?php
+                        include "../include/left-side.php";
+                    ?>
+
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+            <div class="sidebar-footer">
+                <div class="row">
+                    <div class="col-4 link-wrap">
+                        <!-- item-->
+                        <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Settings"><i
+                                class="ti-settings"></i></a>
+                    </div>
+                    <div class="col-4 link-wrap">
+                        <!-- item-->
+                        <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i
+                                class="mdi mdi-gmail"></i></a>
+                    </div>
+                    <div class="col-4 link-wrap">
+                        <!-- item-->
+                        <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i
+                                class="mdi mdi-power"></i></a>
+                    </div>
+                </div>
+            </div>
+        </aside>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-8 align-self-center">
+                        <h3 class="page-title mb-0 p-0">Trang chủ</h3>
+                        <div class="d-flex align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Thống kê</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Sales chart -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <!-- Column -->
+                     
+<div class="col-sm-12">
+                                                    <div class="card">
+                                                        <div class="card-body">
+                                                            <form method="POST" class="form-horizontal form-material mx-2" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                                                            <div class="col-md-10" style="float: left;"> 
+                                                                <div class="input-group"><span style="margin-top: 6px;" class="input-group-addon">Nhập ngày đk online: </span><input name="ngaydkONLINE" placeholder="Ngày đăng ký online: yyyy-mm-dd" class="form-control" type="text">
+                                                                </div>  
+                                                            </div> 
+                                                            <div class="col-md-2" style="float: right;"><button type="submit" name="timngayonl" class="btn btn-success btn-block">Tìm</button></div>
+                                                        </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                             <?php 
+                                            include "../config.php";
+                                            $i=0;
+                                            if(isset($_POST['timngayonl']))
+                                            {
+                                                $ngayonl=addslashes($_POST['ngaydkONLINE']);
+                                                if (!$ngayonl) {
+                                                    echo "Vui lòng nhập ngày cần tìm. <a href='javascript: history.go(-1)'>Trở lại</a>";
+                                                    
+                                                }
+                                                $query = mysql_query("SELECT * FROM nguoihienmau LEFT JOIN hosohienmau on nguoihienmau.MSNHM = hosohienmau.id_NHM  WHERE ngaydkONLM = '$ngayonl' ");
+                                                if (mysql_num_rows($query) == 0) {
+                                                    echo "Ngày đk này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+                                                     
+                                                }
+                                                
+                                            
+                                        ?>
+                                        <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Table Thống kê
+                            </h4>
+                                <div class="table-responsive">
+                                    <form method="POST" class="form-horizontal form-material mx-2" enctype="multipart/form-data" >
+                                        <table class="table user-table" >
+                                            <thead>
+                                                 
+                                                <th>Tổng thể tích máu</th>
+                                                <th>Tổng số người hiến máu</th>
+                                            </thead>
+                                            <?php
+                                                $queryTong ="SELECT sum(thetichmauML) as thetichmau, Count(thetichmauML)as solan
+                                                    FROM nguoihienmau LEFT JOIN hosohienmau on nguoihienmau.MSNHM = hosohienmau.id_NHM  WHERE ngaydkONLM = '$ngayonl'";
+                                                    $sql=mysql_query($queryTong);
+                                                
+                                                while($kq = mysql_fetch_array($sql))
+                                                {
+                                                    $i++;
+                                            ?>
+                                            <tbody>
+                                                <tr>
+                                                    
+                                                    <td style="color: red;"><?php echo $kq['thetichmau']; ?> ml</td>
+                                                    <td style="color: red;"><?php echo $kq['solan']; ?> người</td>
+                                                </tr>
+                                            </tbody>
+                                        <?php } ?>
+                                            <thead>
+                                            <th>STT</th>
+                                            <th>Thể tích máu</th>
+                                            <th>Loại Máu</th>
+                                            <th>Ngày hiến máu</th>
+                                            <th>Người hiến máu</th>
+                                            <th>Số CCCD của NHM</th>
+                                            <th>Thông tin</th>
+                                        </thead>
+                                        <?php 
+                                        
+                                            while($row = mysql_fetch_array($query))
+                                                {
+                                                    $i++;
+                                        ?>
+                                        <tbody>
+                                            <tr>
+                                                <td><?php echo $i; ?></td>
+                                                <td><?php echo $row['thetichmauML']; ?></td>
+                                                <td><?php echo $row['LoaiMau']; ?></td>
+                                                <td><?php echo $row['ngaydkONLM']; ?></td>
+                                                <td><?php echo $row['HOTEN']; ?></td>
+                                                <td><?php echo $row['CCCD']; ?></td>
+                                                <td><a href="pages-DKHM.php?MSNHM=<?php echo $row['MSNHM']; ?>">Xem chi tiết</a></td>
+                                            </tr>
+                                        </tbody>
+                                         <?php }
+                                         ?> 
+                                         </table>
+                                         <?php } ?>
+                                       
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <footer class="footer"> 
+                <?php include "../include/footer.php"; ?>
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="../assets/plugins/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../assets/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/app-style-switcher.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugins -->
+    <!-- ============================================================== -->
+    <!-- chartist chart -->
+    <script src="../assets/plugins/chartist-js/dist/chartist.min.js"></script>
+    <script src="../assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
+    <!--c3 JavaScript -->
+    <script src="../assets/plugins/d3/d3.min.js"></script>
+    <script src="../assets/plugins/c3-master/c3.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/pages/dashboards/dashboard1.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
